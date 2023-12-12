d862c73dd576cbe3ecdbdd0832d06cda56b07c88
Marco <patrickstar1281@yahoo.com>
2023-12-12 11:19:48 -0600

    updated the connection pool

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -8,7 +8,7 @@ from flask import g
 load_dotenv()
 
 # connect to database using env variable
-engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
+engine = create_engine(getenv('DB_URL'), echo=True, pool_size=10, max_overflow=20)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
